86fbcc666dd5f295d871522b32b399a0f2ac9f2b
pablito <pabloivanjuarez@gmail.com>
2020-12-12 20:21:27 -0800

    search && addition routes created

--- backend/routes/API/books.js
+++ backend/routes/API/books.js
@@ -1,2 +1,6 @@
 const router = require('express').Router();
 const controller = require('../../controllers/booksController');
+
+router.route('/')
+  .get(controller.findAll)
+  .put(controller.create);
