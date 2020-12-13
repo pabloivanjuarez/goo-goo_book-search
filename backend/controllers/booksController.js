9a3f0f75cfdfa51cc68fff9a0cbd8261154ebe40
pablito <pabloivanjuarez@gmail.com>
2020-12-12 17:49:28 -0800

    findAll added

--- backend/controllers/booksController.js
+++ backend/controllers/booksController.js
@@ -1,4 +1,12 @@
 const db = require('../models');
 module.exports = {
+  findAll: (req, res) => {
+    db
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+
 
 }
