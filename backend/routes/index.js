5f0c9a0d7d3d8417ca68914ee80a9fca236a73c4
pablito <pabloivanjuarez@gmail.com>
2020-12-12 22:10:05 -0800

    router path layed out

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,3 +1,10 @@
 const router = require('express').Router();
 const path = require('path');
 const APIroutes = require('./API');
+
+router.use('./api', APIroutes);
+
+router.use((req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/public/index.html'));
+});
+
