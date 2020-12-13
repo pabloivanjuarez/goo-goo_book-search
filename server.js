f95c3bbead6bf6ccccbcf7891d15e298e9ae848c
pablito <pabloivanjuarez@gmail.com>
2020-12-12 17:31:47 -0800

    Middleware defined

--- server.js
+++ server.js
@@ -3,3 +3,9 @@ const app = express();
 const routes = require('./backend/routes');
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
