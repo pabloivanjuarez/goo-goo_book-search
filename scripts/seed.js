0c79cb864b1ce11347ab71c545b0c3196cde9099
pablito <pabloivanjuarez@gmail.com>
2020-12-12 21:41:06 -0800

    table filled

--- scripts/seed.js
+++ scripts/seed.js
@@ -7,10 +7,10 @@ mongoose.connect(
 
 const bSeed = [{
 
-  title: '',
-  author: '',
-  description: '',
-  thumbnail: ''
-  link: ''
+  title: 'In God We Trust',
+  author: ['Jean Shepherd'],
+  description: 'The beloved, bestselling classic that became the movie, A CHRISTMAS STORY -- humorous and nostalgic Americana, reissued in a strikingly designed paperback edition. Before Garrison Keillor and Spalding Gray there was Jean Shepherd: a master monologist and writer who spun the materials of his all-American childhood into immensely resonant--and utterly hilarious--works of comic art. In God We Trust: All Others Pay Cash represents one of the peaks of his achievement, a compound of irony, affection, ...',
+  thumbnail: 'http://books.google.com/books/content?id=cHpM9jx9roIC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+  link: 'http://books.google.com/books?id=cHpM9jx9roIC&printsec=frontcover&dq=in+god+we+trust+all+others+pay+cash&hl=&cd=1&source=gbs_api'
 
 }]
