e9758bc9711e08a10fd4c3826d1dc65f4459e58e
pablito <pabloivanjuarez@gmail.com>
2020-12-12 20:26:30 -0800

    rm && get routes created

--- backend/routes/API/books.js
+++ backend/routes/API/books.js
@@ -4,3 +4,7 @@ const controller = require('../../controllers/booksController');
 router.route('/')
   .get(controller.findAll)
   .put(controller.create);
+
+router.route('/:id')
+  .get(controller.findById)
+  .delete(controller.remove);
