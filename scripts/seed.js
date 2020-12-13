8902ccca22f5dfab88f2562a6f2d7aa76873eeaf
pablito <pabloivanjuarez@gmail.com>
2020-12-12 21:11:54 -0800

    seed table created

--- scripts/seed.js
+++ scripts/seed.js
@@ -4,3 +4,13 @@ const db = require('../backend/models');
 mongoose.connect(
   process.env.MONGODB_URI || 'mongod://localhost/googoobooksearch'
 );
+
+const bSeed = [{
+
+  title: '',
+  author: '',
+  description: '',
+  thumbnail: ''
+  link: ''
+
+}]
