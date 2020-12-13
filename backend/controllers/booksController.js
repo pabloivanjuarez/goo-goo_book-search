f2461e261b644b1cbaa26f1f873e6e07bc634782
pablito <pabloivanjuarez@gmail.com>
2020-12-12 17:49:51 -0800

    findById added

--- backend/controllers/booksController.js
+++ backend/controllers/booksController.js
@@ -7,6 +7,11 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-
+  findById: (req, res) => {
+    db
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
 
 }
