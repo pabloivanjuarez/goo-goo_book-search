0979c8d4f7795acb0409a1f1d9213cc6d74217f9
pablito <pabloivanjuarez@gmail.com>
2020-12-15 13:16:35 -0800

    build executed

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -4,6 +4,18 @@ import './styles.modules.css'
 function Navbar() {
   return (
     <nav className='navbar navbar-default-lg'>
+      <div>
+        <ul className='navbar-nav'>
+          <li className='nav-item'>
+            <a className='nav-link' href='/'>Search<img className='nav-icon' src={ }></img>
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='/'>Search<img className='nav-icon' src={ }></img>
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   )
 }
