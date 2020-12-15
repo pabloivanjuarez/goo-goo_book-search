8f412b8c58c2f3fa87d0837ca73eee7e058472db
pablito <pabloivanjuarez@gmail.com>
2020-12-15 15:00:40 -0800

    dependencies added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import SearchBox from './components/SearchBox';
+import SavedList from './components/SavedList';
 
 function App() {
   return (
-    
-  );
-}
+    <Router>
+      );
+      }
 
-export default App;
+      export default App;
